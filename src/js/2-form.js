6fb02cd4aa1079d7591cb0b006b74d25d1ec5f93
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-03-04 15:59:24 +0200

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,34 @@
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+loadLastInfo();
+
+form.addEventListener('submit', submitOnForm);
+form.addEventListener('input', saveUserInfo);
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+function submitOnForm(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function saveUserInfo(event) {
+  formData.email = event.currentTarget.email.value;
+  formData.message = event.currentTarget.message.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function loadLastInfo() {
+  const lastInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  form.elements.email.value = lastInfo.email || '';
+  form.elements.message.value = lastInfo.message || '';
+}
